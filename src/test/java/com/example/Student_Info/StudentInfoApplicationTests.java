3bd45ad0de8f7724f934d6d50807ab121397d358
Asifur Rahman <101541829+asifurrahmancse@users.noreply.github.com>
2022-03-14 11:36:27 +0530

    Add files via upload

---
+++ src/test/java/com/example/Student_Info/StudentInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Student_Info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
